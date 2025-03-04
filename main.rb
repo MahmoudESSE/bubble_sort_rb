edcb00bcd5b26a25d25163c817e6ac0faec15d3a
MahmoudESSE <mahmoudessehayli@gmail.com>
2025-03-04 00:17:34 +0000

    feat: sort array of numbers using bubble sort
    Signed-off-by: MahmoudESSE <mahmoudessehayli@gmail.com>

--- main.rb
+++ main.rb
@@ -1,3 +1,23 @@
 # frozen_string_literal: true
 
-puts 'Hello, World!'
+# Sort an array using bubble sort algorithm
+#
+# @param arr [Array<Integer>] the array to sort
+# @param [Array<Integer>] the sorted array
+def bubble_sort!(arr)
+  (0...arr.length).each do |_|
+    arr.each_with_index do |elem, elem_idx|
+      next if arr[elem_idx + 1].nil? || arr[elem_idx + 1] > elem
+
+      base = arr[elem_idx + 1]
+      old_value = base
+      arr[elem_idx + 1] = arr[elem_idx]
+      arr[elem_idx] = old_value
+    end
+  end
+  arr
+end
+
+arr = (1..1000).to_a.shuffle
+
+pp bubble_sort!(arr)
