fe46f2039aa16760179015dfa3fa3db1077e5a77
MahmoudESSE <mahmoudessehayli@gmail.com>
2025-03-04 00:30:20 +0000

    perf: optimize to not over sort
    Signed-off-by: MahmoudESSE <mahmoudessehayli@gmail.com>

--- main.rb
+++ main.rb
@@ -5,19 +5,28 @@
 # @param arr [Array<Integer>] the array to sort
 # @param [Array<Integer>] the sorted array
 def bubble_sort!(arr)
-  (0...arr.length).each do |_|
-    arr.each_with_index do |elem, elem_idx|
-      next if arr[elem_idx + 1].nil? || arr[elem_idx + 1] > elem
+  return arr if arr.length <= 1
 
-      base = arr[elem_idx + 1]
-      old_value = base
-      arr[elem_idx + 1] = arr[elem_idx]
-      arr[elem_idx] = old_value
+  n = arr.length
+
+  loop do
+    swapped = false
+
+    (0...n - 1).each do |i|
+      if arr[i] > arr[i + 1]
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        swapped = true
+      end
     end
+
+    break unless swapped
+
+    n -= 1
   end
+
   arr
 end
 
 arr = (1..1000).to_a.shuffle
 
-pp bubble_sort!(arr)
+p bubble_sort!(arr)
